3fe77e3b75d3cf4cd5200b23f7e7076b7bd4e51c
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2020-02-03 04:49:38 +0400

    1.0.4 version.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
 // Exporting connect integration.
 module.exports = (auth) => {
     return (req, res, next) => {
-        auth.check(req, res, (req, res, err) => {
+        auth.check((req, res, err) => {
             if (err) {
                 next(err);
             } else {
                 next();
             }
-        });
+        })(req, res);
     }
 };
